ace8ba4262755929222af507bf85f4c3fe580471
AarynLu <hello@0x3f4.run>
2026-02-08 16:40:37 +0800

    feat: Integrate Geist fonts, add Radix Accordion dependency, and adjust logo font weight to medium.

--- apps/landing/src/components/logo.tsx
+++ apps/landing/src/components/logo.tsx
@@ -7,7 +7,7 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={cn('flex items-center gap-3', className)}>
       <LogoSvg className='size-8.5 text-primary' />
-      <span className='text-primary text-[20px] font-semibold uppercase'>Atmos</span>
+      <span className='text-primary text-[20px] font-medium uppercase'>Atmos</span>
     </div>
   )
 }
